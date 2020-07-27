da8f5e20d4d2972ba730eac7969ab1cbe9c88fe1
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-27 10:28:22 -0600

    Added onclick event listener for button on form.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import React from 'react';
 
 class App extends React.Component
 {
+  constructor (props)
+  {
+    super(props);
+    this.state = {
+      newToDo: "", //Keep track of our new to-do value.
+      toDos: []  //keep track of all the todos.
+    };
+  }
+
+  addToDO(event)
+   {
+    event.preventDefault();
+    console.log("Test add todo!");
+  }
   render()
   {
     return(
 
       <>
       <h1>React To-Do App</h1>
-      <form>
+      <form onSubmit={this.addToDO}>
         <label htmlFor="newToDo">
           Enter a new "To-Do:"
           <input type="text" name ="newToDo" id="newToDo" required />
